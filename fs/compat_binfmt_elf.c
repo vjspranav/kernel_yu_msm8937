cd33856f5df64c6615e3bb096320c3f99aa5fc69
Shoaib Anwar <Shoaib0595@gmail.com>
2018-05-07 17:05:15 +0530

    FS/COMPAT_BINFMT_ELF: Fix "Defined but Un-Used" Error
    Signed-off-by: Shoaib Anwar <Shoaib0595@gmail.com>

--- fs/compat_binfmt_elf.c
+++ fs/compat_binfmt_elf.c
@@ -51,6 +51,8 @@
 #define elf_prstatus	compat_elf_prstatus
 #define elf_prpsinfo	compat_elf_prpsinfo
 
+
+#ifdef CONFIG_ELF_CORE
 /*
  * Compat version of cputime_to_compat_timeval, perhaps this
  * should be an inline in <linux/compat.h>.
@@ -64,8 +66,10 @@ static void cputime_to_compat_timeval(const cputime_t cputime,
 	value->tv_usec = tv.tv_usec;
 }
 
+
 #undef cputime_to_timeval
 #define cputime_to_timeval cputime_to_compat_timeval
+#endif
 
 
 /*
